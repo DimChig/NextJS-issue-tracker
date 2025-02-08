3b6711ce21d1ba98b1f1aba20cced51da348abdd
DimChig <dima.chigarev@gmail.com>
2025-02-08 02:48:33 -0500

    fixed bug: switched to dynamic rendering of SimpleMDE

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { Button, Callout, Text, TextArea, TextField } from "@radix-ui/themes";
-import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
 import { useForm, Controller } from "react-hook-form";
 import axios from "axios";
@@ -12,6 +11,10 @@ import { createIssueSchema } from "@/app/validationSchemas";
 import { z } from "zod";
 import ErrorMessage from "@/app/components/ErrorMessage";
 import Spinner from "@/app/components/Spinner";
+const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
+  ssr: false,
+});
+import dynamic from "next/dynamic";
 
 type IssueForm = z.infer<typeof createIssueSchema>;
 
