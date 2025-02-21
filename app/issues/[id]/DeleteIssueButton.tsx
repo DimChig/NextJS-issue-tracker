ff1a5ddadfc5830437da43a993b87398c6f20947
DimChig <dima.chigarev@gmail.com>
2025-02-20 21:25:02 -0500

    Handle errors

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -5,43 +5,71 @@ import { AlertDialog } from "radix-ui";
 import "@/app/style.css";
 import axios from "axios";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
   const router = useRouter();
+  const [error, setError] = useState(false);
+
+  const deleteIssue = async () => {
+    try {
+      await axios.delete("/api/issues/" + issueId);
+      router.push("/issues");
+      router.refresh();
+    } catch (error) {
+      setError(true);
+    }
+  };
+
   return (
-    <AlertDialog.Root>
-      <AlertDialog.Trigger asChild>
-        <Button color="red">Delete Issue</Button>
-      </AlertDialog.Trigger>
-      <AlertDialog.Portal>
-        <AlertDialog.Overlay className="AlertDialogOverlay" />
-        <AlertDialog.Content className="AlertDialogContent">
-          <AlertDialog.Title className="AlertDialogTitle">
-            Are you absolutely sure?
-          </AlertDialog.Title>
-          <AlertDialog.Description className="AlertDialogDescription">
-            This action cannot be undone. This will permanently delete the issue
-          </AlertDialog.Description>
-          <div style={{ display: "flex", gap: 25, justifyContent: "flex-end" }}>
-            <AlertDialog.Cancel asChild>
-              <button className="Button mauve">Cancel</button>
-            </AlertDialog.Cancel>
-            <AlertDialog.Action asChild>
-              <button
-                className="Button red"
-                onClick={async () => {
-                  await axios.delete("/api/issues/" + issueId);
-                  router.push("/issues");
-                  router.refresh();
-                }}
-              >
-                Delete Issue
-              </button>
-            </AlertDialog.Action>
-          </div>
-        </AlertDialog.Content>
-      </AlertDialog.Portal>
-    </AlertDialog.Root>
+    <>
+      <AlertDialog.Root>
+        <AlertDialog.Trigger asChild>
+          <Button color="red">Delete Issue</Button>
+        </AlertDialog.Trigger>
+        <AlertDialog.Portal>
+          <AlertDialog.Overlay className="AlertDialogOverlay" />
+          <AlertDialog.Content className="AlertDialogContent">
+            <AlertDialog.Title className="AlertDialogTitle">
+              Are you absolutely sure?
+            </AlertDialog.Title>
+            <AlertDialog.Description className="AlertDialogDescription">
+              This action cannot be undone. This will permanently delete the
+              issue
+            </AlertDialog.Description>
+            <div
+              style={{ display: "flex", gap: 25, justifyContent: "flex-end" }}
+            >
+              <AlertDialog.Cancel asChild>
+                <button className="Button mauve">Cancel</button>
+              </AlertDialog.Cancel>
+              <AlertDialog.Action asChild>
+                <button className="Button red" onClick={deleteIssue}>
+                  Delete Issue
+                </button>
+              </AlertDialog.Action>
+            </div>
+          </AlertDialog.Content>
+        </AlertDialog.Portal>
+      </AlertDialog.Root>
+
+      <AlertDialog.Root open={error}>
+        <AlertDialog.Portal>
+          <AlertDialog.Overlay className="AlertDialogOverlay" />
+          <AlertDialog.Content className="AlertDialogContent">
+            <AlertDialog.Title className="AlertDialogTitle">
+              Error
+            </AlertDialog.Title>
+            <AlertDialog.Description className="AlertDialogDescription">
+              This issue could not be deleted.
+            </AlertDialog.Description>
+            <button className="Button mauve" onClick={() => setError(false)}>
+              OK
+            </button>
+          </AlertDialog.Content>
+        </AlertDialog.Portal>
+      </AlertDialog.Root>
+    </>
   );
 };
 
