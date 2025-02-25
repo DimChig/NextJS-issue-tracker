f54746465a580eae2992287cbb442739ab2e72fe
DimChig <dima.chigarev@gmail.com>
2025-02-24 23:16:51 -0500

    Enchance the API to assign issues

--- app/api/issues/[id]/route.tsx
+++ app/api/issues/[id]/route.tsx
@@ -1,8 +1,9 @@
-import { issueSchema } from "@/app/validationSchemas";
 import prisma from "@/lib/prisma";
 import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
 import { authOptions } from "../../auth/[...nextauth]/route";
+import { error } from "console";
+import { patchIssueSchema } from "@/app/validationSchemas";
 
 type Params = Promise<{ id: string }>;
 
@@ -21,10 +22,20 @@ export async function PATCH(
   const issueId = params.id;
 
   const body = await request.json();
-
-  const validation = issueSchema.safeParse(body);
+  const validation = patchIssueSchema.safeParse(body);
   if (!validation.success) {
     return NextResponse.json(validation.error.format(), { status: 400 });
+  }
+
+  const { title, description, assignedToUserId } = body;
+  if (assignedToUserId) {
+    const user = await prisma.user.findUnique({
+      where: {
+        id: assignedToUserId,
+      },
+    });
+    if (!user)
+      return NextResponse.json({ error: "Invalid user" }, { status: 400 });
   }
 
   const issue = await prisma.issue.findUnique({
@@ -38,8 +49,9 @@ export async function PATCH(
   const updatedIssue = await prisma.issue.update({
     where: { id: issue.id },
     data: {
-      title: body.title,
-      description: body.description,
+      title: title,
+      description: description,
+      assignedToUserId: assignedToUserId,
     },
   });
   return NextResponse.json(updatedIssue);
@@ -52,7 +64,7 @@ export async function DELETE(
   const session = await getServerSession(authOptions);
   if (!session)
     return NextResponse.json(
-      { message: "You must be logged in." },
+      { message: "You must be logged in" },
       { status: 401 }
     );
 
