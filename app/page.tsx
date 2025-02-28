40feea92bba2ad32d34897c14c4785ae12ba8f23
DimChig <dima.chigarev@gmail.com>
2025-02-27 23:02:45 -0500

    build the LatestIssues component

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,6 @@
 import Pagination from "./components/Pagination";
+import LatestIssues from "./LatestIssues";
 
-export default async function Home({
-  searchParams,
-}: {
-  searchParams: { page: string };
-}) {
-  const searchParamsObject = await searchParams;
-  return (
-    <Pagination
-      itemCount={100}
-      pageSize={10}
-      currentPage={parseInt(searchParamsObject.page)}
-    />
-  );
+export default function Home() {
+  return <LatestIssues />;
 }
